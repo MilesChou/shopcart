31190244f2b32683ef6f870c9d381a3fe8ab81f4
MilesChou <jangconan@gmail.com>
2016-09-10 00:01:58 +0800

    Initial Slim and Tests

---
+++ app.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$container = new Slim\Container();
+
+$app = new \Slim\App($container);
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $user = new Creative\User($name);
+    $greeting = $user->sayHello();
+
+    $response->getBody()->write($greeting);
+
+    return $response;
+});
+
+return $app;
