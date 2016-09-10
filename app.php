c50e4890d915b8ab0e845799a3a155bc391acc4b
MilesChou <jangconan@gmail.com>
2016-09-11 02:53:26 +0800

    Fix router to slim

--- app.php
+++ app.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 require __DIR__ . '/vendor/autoload.php';
 
 $container = new Slim\Container();
+$container['displayErrorDetails'] = true;
 
 $app = new \Slim\App($container);
 
@@ -18,6 +19,18 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     $response->getBody()->write($greeting);
 
     return $response;
+});
+
+$app->get('/', function (Request $request, Response $response) {
+    require __DIR__ . '/index.php';
+});
+
+$app->get('/index.php', function (Request $request, Response $response) {
+    require __DIR__ . '/index.php';
+});
+
+$app->get('/admin.php', function (Request $request, Response $response) {
+    require __DIR__ . '/admin.php';
 });
 
 return $app;
